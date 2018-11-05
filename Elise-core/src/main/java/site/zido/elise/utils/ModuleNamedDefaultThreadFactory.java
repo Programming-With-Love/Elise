efe8c6d04b9c6d6ddaa79242a0f2be83132daaab
zido <wuhongxu1208@gmail.com>
2018-11-05 16:43:31 +0800

    add log to default thread factory

--- Elise-core/src/main/java/site/zido/elise/utils/ModuleNamedDefaultThreadFactory.java
+++ Elise-core/src/main/java/site/zido/elise/utils/ModuleNamedDefaultThreadFactory.java
@@ -1,5 +1,8 @@
 package site.zido.elise.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -10,12 +13,14 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class ModuleNamedDefaultThreadFactory implements ThreadFactory {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ModuleNamedDefaultThreadFactory.class);
     private static final AtomicInteger poolNumber = new AtomicInteger(1);
-    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private volatile int threadNumber = 1;
     private final ThreadGroup group;
     //will be a part of name
     private final String prefix;
     private boolean daemon;
+    private String moduleName;
 
     public ModuleNamedDefaultThreadFactory(String moduleName) {
         this(moduleName, false);
@@ -26,14 +31,24 @@ public class ModuleNamedDefaultThreadFactory implements ThreadFactory {
         group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
         prefix = moduleName + "-pool-" + poolNumber.getAndIncrement() + "-thread-";
         this.daemon = daemon;
+        this.moduleName = moduleName;
     }
 
     @Override
     public Thread newThread(Runnable r) {
-        Thread t = new Thread(group, r, prefix + threadNumber.getAndIncrement(), 0);
+        StringBuilder sb = new StringBuilder();
+        Thread t;
+        synchronized (this) {
+            int number = threadNumber;
+            threadNumber++;
+            t = new Thread(group, r, prefix + number, 0);
+            sb.append("create a thread of ").append(moduleName);
+        }
         t.setDaemon(daemon);
+        sb.append(",daemon:").append(daemon);
         if (t.getPriority() != Thread.NORM_PRIORITY)
             t.setPriority(Thread.NORM_PRIORITY);
+        LOGGER.debug(sb.toString());
         return t;
     }
 }
